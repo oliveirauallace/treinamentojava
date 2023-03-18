a02a7e888018edc2c10d5fa19e7633f411f56399
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-17 21:38:36 -0300

    Novo

--- Exercicio12.java
+++ Exercicio12.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Exercicio12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int num = sc.nextInt();
+        if (num <= 0){
+            num = num *(-1);
+            System.out.println("O módulo do número digitado é: "+num);
+        }else {
+            System.out.println("O módulo do número digitado é: "+num);
+        }
 
     }
 }
---
+++ Exercicio13.java
@@ -0,0 +1,19 @@
+package Exercícios;
+
+import java.util.Scanner;
+
+public class Exercicio13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o primeiro número: ");
+        int num1 = sc.nextInt();
+        System.out.println("Informe o primeiro número: ");
+        int num2 = sc.nextInt();
+        System.out.println("Informe o primeiro número: ");
+        int num3 = sc.nextInt();
+
+        if (num1 > num2){
+
+        }
+    }
+}
