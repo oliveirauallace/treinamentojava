bb517fb9f4d0c3e9e5be2e98cfabee3c490dff4d
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-18 18:20:28 -0300

    Last Commit

---
+++ Exercicio18.java
@@ -0,0 +1,10 @@
+package Exercícios;
+
+import java.util.Scanner;
+
+public class Exercicio18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("");
+    }
+}
