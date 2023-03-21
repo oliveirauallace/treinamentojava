3ffa656c97c15fe398cc0504b6e939e0b8e39d3e
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-20 21:14:49 -0300

    Last Commit

--- Exercicio18.java
+++ Exercicio18.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class Exercicio18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("Informe um número: ");
+        int num = sc.nextInt();
+        switch (num){
+            case 1:
+            System.out.println("Um");
+            break;
+            case 2:
+                System.out.println("Dois");
+                break;
+            case 3:
+                System.out.println("Três");
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
     }
 }
--- Exercicio19.java
+++ Exercicio19.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 public class Exercicio19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o primeiro valor: ");
+        int A = sc.nextInt();
+        System.out.println("informe o segundo valor: ");
+        int B = sc.nextInt();
+        System.out.println("Informe o terceiro valor: ");
+        int C = sc.nextInt();
+
+        if (A < B+C){
+            if (A==B && A == C){
+                System.out.println("Triangulo Equilátero");
+            } else if (A ==B || A==C || B ==C) {
+                System.out.println("Triangulo isórceles");
+
+            }else {
+                System.out.println("Triangulo escaleno");
+            }
+        }else {
+            System.out.println("Não se caracteriza um triangulo");
+        }
 
     }
 }
