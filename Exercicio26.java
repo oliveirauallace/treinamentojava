097afe1c782c0147c277325a5afab625793e2916
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-21 21:24:57 -0300

    Last Commit

---
+++ Exercicio26.java
@@ -0,0 +1,14 @@
+package Exercícios;
+
+public class Exercicio26 {
+    public static void main(String[] args) {
+//        for (int num = 100; num>=0; num--){
+//            System.out.println(num);
+//        }
+        int num = 100;
+        while (num >=0){
+            num--;
+            System.out.println(num);
+        }
+    }
+}
