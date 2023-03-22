cd400f9856063d46a3b2e08b95765b22d300d91c
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-22 20:31:33 -0300

    Last Commit

---
+++ Exercicio28.java
@@ -0,0 +1,12 @@
+package Exercícios;
+
+public class Exercicio28 {
+    public static void main(String[] args) {
+        int num;
+        for (num = 0; num <=200; num++){
+            if (num%2!=0){
+                System.out.println(num);
+            }
+        }
+    }
+}
