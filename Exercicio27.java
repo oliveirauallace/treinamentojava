40f9a5898b778df5b26f948bcd0ba20bd621c3c5
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-22 19:44:13 -0300

    Last Commit

--- Exercicio27.java
+++ Exercicio27.java
@@ -6,7 +6,20 @@ public class Exercicio27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Informe a quantidade de número que deve ser processado: ");
-        int num = sc.nextInt();
+        int quantidade = sc.nextInt();
+
+        long[] num = new long[quantidade];
+        for (int x=0; x< num.length; x++){
+            System.out.println("Digite um número");
+            num [x] = sc.nextInt();
+        }
+        for (int x=0; x < num.length; x++){
+            long resultado =1;
+            for (long y=num[x]; y>0; y--){
+                resultado = y* resultado;
+            }
+            System.out.println("O fatorial é: "+num[x]+" é: "+resultado);
+        }
 
         
     }
