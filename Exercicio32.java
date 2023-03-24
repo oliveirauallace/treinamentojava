f1e94e9f9741c12b5497be03fe7a7afd63625056
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-23 22:23:33 -0300

    Last Commit

--- Exercicio32.java
+++ Exercicio32.java
@@ -1,7 +1,11 @@
 package Exercícios;
 
+import java.util.Scanner;
+
 public class Exercicio32 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
 
     }
 }
