5f5d1fa7f1e38bbcfbd45d19fe780c879f24d4f0
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-24 22:20:02 -0300

    Last Commit

--- Exercicio32.java
+++ Exercicio32.java
@@ -1,19 +1,27 @@
 package Exercícios;
 
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class Exercicio32 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num;
+        int resultado =0;
 
 
-        System.out.println("Digite um número:");
-        int num = sc.nextInt();
+        do{
+            System.out.println("Digite um número maior que zero e menor que 10");
+            num = sc.nextInt();
 
-        while (num >0 || num<10){
-            for ()
 
+        }while (num >=0 || num >=10);
+        for (int x=num; x<4+num;x++){
+            if (x%2==1){
+                 resultado += x*x;
+
+            }
         }
-
+        System.out.println("O resultado da soma dos quadrados é: "+resultado);
     }
 }
