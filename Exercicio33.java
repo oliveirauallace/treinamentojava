7b00eb7721138989b368543ea34ccdcaf98c0e3c
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-24 22:58:09 -0300

    Last Commit

---
+++ Exercicio33.java
@@ -0,0 +1,24 @@
+package Exercícios;
+
+import java.util.Scanner;
+
+public class Exercicio33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double matricula;
+        double nota;
+        int cont;
+
+        System.out.println("Informe a matrícula do aluno: ");
+        matricula = sc.nextDouble();
+        System.out.println("Informe a nota do aluno:");
+        nota = sc.nextDouble();
+
+        while(matricula != 0) {
+            double media = nota;
+
+
+            }
+
+    }
+}
