8ef1ee256f2a2097c5e55856683655f4da36015b
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-25 22:57:40 -0300

    Last Commit

--- Exercicio34.java
+++ Exercicio34.java
@@ -1,7 +1,23 @@
 package Exercícios;
 
+import java.util.Scanner;
+
 public class Exercicio34 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num =0;
+
+        do {
+            System.out.println("Informe quantos números quiser, número negativo encerra o programa");
+            num = sc.nextInt();
+            if (num>0){
+
+            }
+
+
+
+        }while (num <0);
+        System.out.println(num);
 
     }
 }
