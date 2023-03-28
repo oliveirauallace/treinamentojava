c716c64a672d2bb07efdb81d468340ae091e63af
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-27 21:43:27 -0300

    Last Commit

--- Exercicio36.java
+++ Exercicio36.java
@@ -1,31 +1,28 @@
 package Exercícios;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercicio36 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numAtual, maior, menor, med;
+        int quantidadeNum = 10;//Setando a quantidade dos números informados
+        double media =0;
+        int [] num = new int[quantidadeNum];
 
-        System.out.println("Digite os números: ");
-        numAtual = sc.nextInt();
+        for (int x = 0; x < quantidadeNum; x++) {
+            System.out.println("Digite o "+(x+1)+"º número");
+            num[x]= sc.nextInt();
+            media += num[x];
 
-
-
-        if (numAtual >0){
-            maior = numAtual;
-            menor = numAtual;
-            while (numAtual >0){
-                if (numAtual > maior){
-                    maior = numAtual;
-                }else if (numAtual < menor && numAtual >=0) {
-                    menor = numAtual;
-                }
-                double md = numAtual/10;
-
-                System.out.println(maior +" é o númerio Maior,"+menor+" é o número menor"+md);
-            }
 
         }
+        media = media / quantidadeNum;
+        Arrays.sort(num);
+        System.out.println("O maior número é: "+num[0]);
+        System.out.println("O menor número é: "+num[quantidadeNum -1]);
+        System.out.println("A média dos números é: "+ media);
+
+
     }
 }
