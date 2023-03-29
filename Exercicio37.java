6aa07e5cb39d84e1d01568d4f388b9fb351cbd93
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-28 22:15:59 -0300

    Last Commit

--- Exercicio37.java
+++ Exercicio37.java
@@ -1,7 +1,27 @@
 package Exercícios;
 
+import java.util.Scanner;
+
 public class Exercicio37 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+        int num1;
+        int num2;
 
+        do {
+            System.out.println("Seleicone as seguintes opções:");
+            System.out.println("1 - Adição, 2 - Subtração, 3 - Multplicação, 4 - Divisão");
+            opcao = sc.nextInt();
+            if (opcao == 1){
+                System.out.println("Informe o primeiro número: ");
+                num1 = sc.nextInt();
+                System.out.println("Informe o segundo número: ");
+                num2 = sc.nextInt();
+
+
+            }
+
+        }
     }
 }
