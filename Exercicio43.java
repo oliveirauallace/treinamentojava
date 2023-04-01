4f5da70f60d0504c0abc602e39c9af75e98bbd2d
Uallace Oliveira <uallace.oliveiraa@gmail.com>
2023-03-31 22:41:17 -0300

    Last commit

--- Exercicio43.java
+++ Exercicio43.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Exercicio43 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double primeiro, segundo;
         double graosporcasa =1, casasdotabuleiro = 64;
         int graos =1;
         for (int i = 1; i < casasdotabuleiro; i++) {
